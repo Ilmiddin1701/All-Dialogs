afae41fcbe1731bccd6bba0ff4d49559ce71674d
Ilmiddin O'rinov <ilmiddinorino1@gmail.com>
2024-07-18 06:06:09 -0700

    all dialogs

---
+++ app/src/main/java/uz/ilmiddin1701/dialogs/MainActivity.kt
@@ -0,0 +1,67 @@
+package uz.ilmiddin1701.dialogs
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import uz.ilmiddin1701.dialogs.databinding.ActivityMainBinding
+import uz.ilmiddin1701.dialogs.databinding.CustomDialogItemBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+//        val snackBar = Snackbar.make(it, "Salom snackbar", Snackbar.LENGTH_LONG)
+//        snackBar.setAction("Bekor qilish") {
+//            Toast.makeText(this@MainActivity, "Bekor qilindi", Toast.LENGTH_SHORT).show()
+//        }
+//        snackBar.show()
+
+        binding.apply {
+            alertDialog.setOnClickListener {
+                val alertDialog = AlertDialog.Builder(this@MainActivity).create()
+                alertDialog.setTitle("Salom")
+                alertDialog.setMessage("Alert Dialog")
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "YES") { dialog, _ ->
+                    Toast.makeText(this@MainActivity, "AlertDialog👍", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "NO") { dialog, _ ->
+                    Toast.makeText(this@MainActivity, "AlertDialog👎", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "CANCEL") { dialog, _ ->
+                    Toast.makeText(this@MainActivity, "AlertDialog❌", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+                alertDialog.show()
+            }
+
+            customDialog.setOnClickListener {
+                val customDialog = AlertDialog.Builder(this@MainActivity).create()
+                val dialogView = CustomDialogItemBinding.inflate(layoutInflater)
+                dialogView.apply {
+                    btnSave.setOnClickListener {
+                        if (edtEmailDialog.text.isNotBlank() && edtPasswordDialog.text.isNotBlank()) {
+                            Toast.makeText(this@MainActivity, "Saqlandi", Toast.LENGTH_SHORT).show()
+                            customDialog.dismiss()
+                        } else {
+                            Toast.makeText(this@MainActivity, "Ma'lumotlar to'liq emas", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                customDialog.setView(dialogView.root)
+                customDialog.show()
+            }
+        }
+    }
+}
